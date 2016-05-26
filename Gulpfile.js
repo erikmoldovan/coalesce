e8b1ecf51ed79284df0431fdbc780c07c2359f98
Erik Moldovan <erikmoldovan@gmail.com>
2016-05-26 09:19:38 -0700

    Removing API, porting to React

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,6 @@
 // Module Includes
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
-	nodemon = require('gulp-nodemon'),
 	sass = require('gulp-sass'),
 	notify = require('gulp-notify');
 
@@ -12,22 +11,6 @@ gulp.task('map:connect', function() {
 		port: 8000,
 		livereload: true
 	});
-});
-
-gulp.task('api:connect', function() {
-	nodemon({
-        // the script to run the app
-        script: 'api/server.js',
-        // this listens to changes in any of these files/routes and restarts the application
-        watch: ["api/server.js"],
-        ext: 'js'
-        // Below i'm using es6 arrow functions but you can remove the arrow and have it a normal .on('restart', function() { // then place your stuff in here }
-    }).on('restart', () => {
-	    gulp.src('server.js')
-	        // I've added notify, which displays a message on restart. Was more for me to test so you can remove this
-	        .pipe(notify('Running the start tasks and stuff'));
-	    }
-	);
 });
 
 gulp.task('html', function() {
@@ -51,5 +34,5 @@ gulp.task('sass:watch', function () {
 });
 
 // Master tasks
-gulp.task('serve', ['html', 'sass', 'map:connect', 'api:connect', 'html:watch', 'sass:watch']);
+gulp.task('serve', ['html', 'sass', 'map:connect', 'html:watch', 'sass:watch']);
 gulp.task('default', ['serve']);
