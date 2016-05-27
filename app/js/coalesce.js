68d86c5354e5a6068dc8005c33245f7ce0403a48
Vexman <vexman1@gmail.com>
2016-05-26 23:05:32 -0400

    sidebar dynamic information update

--- app/js/coalesce.js
+++ app/js/coalesce.js
@@ -36,33 +36,41 @@ var Map = React.createClass({
   }
 });
 
-var TopViewNav = React.createClass({	
+var TopViewNav = React.createClass({ 
   displayName: "TopViewNav",
   render: function(){
-    return (
-		<div id="top-view-nav">
-			<div className="event-name">Bliss Wisdom Summer Fair for Father's Day!</div>
-			<div className="event-date-time">
-				<div className="event-date">Sunday, June 19, 2016</div>
-				<div className="event-time">11:00 AM to 5:00 PM</div>
-			</div>
-			<div className="event-location">
-				<div className="location-name">Queens College parking lot #15 (Colden Auditorium)</div>
-				<div className="location-address">65-30 Kissena Blvd, Flushing, NY</div>
-			</div>
-			<div className="event-description">Eat healthy, protect lives, save the planet!
-				<br/>
-				Outdoor games &crafts, amazing entertainment, bouncers, and healthy foods. Great for families to celebrate Father's Day! Please text Sherry at[masked] to sign up and purchase food vouchers.
-			</div>
-		</div>
-    )
-  }
+    var selected = this.props.data.results[0];
+    if (selected === undefined) {
+      return (<div></div>);
+    } else {
+     console.log(selected);
+        var dur = selected.duration === undefined ? 0 : selected.duration + selected.time;
+        var CalDate = moment(selected.time).format("dddd, MMMM DD, YYYY");
+        var Time = moment(selected.time).format("h:mm A");
+        var Duration = dur === 0 ? "" : "to " + moment(dur).format("h:mm A");
+        return (
+          <div id="top-view-nav">
+            <div className="event-name">{selected.name}</div>
+            <div className="event-date-time">
+              <div className="event-date">{CalDate}</div>
+              <div className="event-time">{Time} {Duration}</div>
+            </div>
+            <div className="event-location">
+              <div className="location-name">{selected.venue.name || ""}</div>
+              <div className="location-address">{selected.venue.address_1 || ""} {selected.venue.address_2 || ""} {selected.venue.address_3 || ""}, {selected.venue.city}, {selected.venue.state}</div>
+            </div>
+            <div className="event-description">{selected.description}</div>
+          </div>
+        )
+      }
+    }
 });
 
 var ShowList = React.createClass({
   render: function(){
-    var listItems = this.props.data.results.map(function(friend, index){
-      return <Event key={index} data={friend}/>;
+    this.props.data.results.shift();
+    var listItems = this.props.data.results.map(function(EventItem, index){
+      return <Event key={index} data={EventItem}/>;
     });
     return (
       <div>
@@ -76,12 +84,10 @@ var Event = React.createClass({
   render: function() {
   var EventItem = this.props.data;
 
-  var dur = EventItem.duration === undefined ? 0 : EventItem.duration + EventItem.time
+  var dur = EventItem.duration === undefined ? 0 : EventItem.duration + EventItem.time;
   var CalDate = moment(EventItem.time).format("dddd, MMMM DD, YYYY");
   var Time = moment(EventItem.time).format("h:mm A");
   var Duration = dur === 0 ? "" : "to " + moment(dur).format("h:mm A");
-
-  /*var name = EventItem.venue === undefined ? EventItem.group.name : EventItem.venue.name;*/
     return (
         <div className="event-item">
           <div className="title">{EventItem.name}</div>
@@ -122,7 +128,6 @@ var PageRender = React.createClass({
       cache: false,
       success: function(data) {
         this.setState({ data: data });
-        console.log(data)
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -140,7 +145,7 @@ var PageRender = React.createClass({
 	    	<div id="main-wrapper">
 	    		<main><Map /></main>
 	    		<aside id="content-nav">
-					<TopViewNav />
+					<TopViewNav data={this.state.data}/>
 					<BottomViewNav data={this.state.data}/>
 				</aside>
 			</div>
