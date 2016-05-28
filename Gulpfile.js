238217fe3dec80594130ca6a2b38070ef61ed111
Erik Moldovan <erikmoldovan@gmail.com>
2016-05-27 18:09:06 -0700

    Adding React Leaflet and reinstituting API layer

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	sass = require('gulp-sass'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+	nodemon = require('gulp-nodemon');
 
 // Base tasks
 gulp.task('map:connect', function() {
@@ -11,6 +12,22 @@ gulp.task('map:connect', function() {
 		port: 8000,
 		livereload: true
 	});
+});
+
+gulp.task('api:connect', function() {
+	nodemon({
+        // the script to run the app
+        script: 'api/server.js',
+        // this listens to changes in any of these files/routes and restarts the application
+        watch: ["api/server.js"],
+        ext: 'js'
+        // Below i'm using es6 arrow functions but you can remove the arrow and have it a normal .on('restart', function() { // then place your stuff in here }
+    }).on('restart', () => {
+	    gulp.src('server.js')
+	        // I've added notify, which displays a message on restart. Was more for me to test so you can remove this
+	        .pipe(notify('Running the start tasks and stuff'));
+	    }
+	);
 });
 
 gulp.task('html', function() {
@@ -43,5 +60,5 @@ gulp.task('js:watch', function () {
 });
 
 // Master tasks
-gulp.task('serve', ['html', 'sass', 'js', 'map:connect', 'html:watch', 'sass:watch', 'js:watch']);
+gulp.task('serve', ['html', 'sass', 'js','map:connect', 'api:connect', 'html:watch', 'sass:watch', 'js:watch']);
 gulp.task('default', ['serve']);
---
+++ api/server.js
@@ -0,0 +1,101 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+// var bodyParser = require('body-parser');
+// var querystring = require('querystring');
+// var https =      require('https');
+var meetup      = require('meetup-api')({
+	key: '1142381b68362c61371669687e3d6db' 
+});
+
+// configure app to use bodyParser()
+// // this will let us get the data from a POST
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+var port = process.env.PORT || 9000;        // set our port
+// var host = "api.meetup.com";
+// var apiKey = "1142381b68362c61371669687e3d6db";
+
+// // Endpoints
+// var endpoints = {
+// 	open_events: "/2/open_events"
+// };
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+
+    var parameters = {
+    	zip: '98122'
+    };
+
+    console.log(parameters);
+
+    meetup.getStreamOpenEvents(parameters, function(err, resp) {
+	    console.log(err, resp);
+	});
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
+
+
+
+// function performRequest(endpoint, method, data, success) {
+//   var dataString = JSON.stringify(data);
+//   var headers = {};
+  
+//   if (method == 'GET') {
+//     endpoint += '?' + querystring.stringify(data);
+//   }
+//   else {
+//     headers = {
+//       'Content-Type': 'application/json',
+//       'Content-Length': dataString.length
+//     };
+//   }
+//   var options = {
+//     host: host,
+//     path: endpoint,
+//     method: method,
+//     headers: headers
+//   };
+
+//   var req = https.request(options, function(res) {
+//     res.setEncoding('utf-8');
+
+//     var responseString = '';
+
+//     res.on('data', function(data) {
+//       responseString += data;
+//     });
+
+//     res.on('end', function() {
+//       console.log(responseString);
+//       var responseObject = JSON.parse(responseString);
+//       success(responseObject);
+//     });
+//   });
+
+//   req.write(dataString);
+//   req.end();
+// }
