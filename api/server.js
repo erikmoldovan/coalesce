7d37a2623597539da2058cfbc3ce5bfe37428d0d
Erik Moldovan <erikmoldovan@gmail.com>
2016-05-27 19:23:55 -0700

    API hooked up properly with Meetup

--- api/server.js
+++ api/server.js
@@ -6,26 +6,11 @@
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
-// var bodyParser = require('body-parser');
-// var querystring = require('querystring');
-// var https =      require('https');
-var meetup      = require('meetup-api')({
-	key: '1142381b68362c61371669687e3d6db' 
+var meetup = require('meetup-api')({
+	key: '1142381b68362c61371669687e3d6db'
 });
 
-// configure app to use bodyParser()
-// // this will let us get the data from a POST
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
 var port = process.env.PORT || 9000;        // set our port
-// var host = "api.meetup.com";
-// var apiKey = "1142381b68362c61371669687e3d6db";
-
-// // Endpoints
-// var endpoints = {
-// 	open_events: "/2/open_events"
-// };
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -34,15 +19,17 @@ var router = express.Router();              // get an instance of the express Ro
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
+});
 
-    var parameters = {
-    	zip: '98122'
-    };
-
-    console.log(parameters);
-
-    meetup.getStreamOpenEvents(parameters, function(err, resp) {
-	    console.log(err, resp);
+router.get('/getEvents', function(req, res) {
+    meetup.getOpenEvents({
+		zip: '98122'
+	}, function(error, events) {
+		if (error) {
+			console.log(error);
+		} else {
+			res.json(events);
+		}
 	});
 });
 
@@ -56,46 +43,3 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
-
-
-
-
-// function performRequest(endpoint, method, data, success) {
-//   var dataString = JSON.stringify(data);
-//   var headers = {};
-  
-//   if (method == 'GET') {
-//     endpoint += '?' + querystring.stringify(data);
-//   }
-//   else {
-//     headers = {
-//       'Content-Type': 'application/json',
-//       'Content-Length': dataString.length
-//     };
-//   }
-//   var options = {
-//     host: host,
-//     path: endpoint,
-//     method: method,
-//     headers: headers
-//   };
-
-//   var req = https.request(options, function(res) {
-//     res.setEncoding('utf-8');
-
-//     var responseString = '';
-
-//     res.on('data', function(data) {
-//       responseString += data;
-//     });
-
-//     res.on('end', function() {
-//       console.log(responseString);
-//       var responseObject = JSON.parse(responseString);
-//       success(responseObject);
-//     });
-//   });
-
-//   req.write(dataString);
-//   req.end();
-// }
