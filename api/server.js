243b33548102272e41e4657daec9322f0f1aba37
Erik Moldovan <erikmoldovan@gmail.com>
2016-06-03 15:39:05 -0700

    Abstracted key, added TODO items list and blah

--- api/server.js
+++ api/server.js
@@ -6,9 +6,23 @@
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
-var meetup = require('meetup-api')({
-	key: '1142381b68362c61371669687e3d6db'
+var fs         = require('fs');
+var meetup;
+
+fs.readFile('./api/key.txt', 'utf8', function read(err, data) {
+    if (err) throw err;
+    key = data;
+
+    linkWithMeetup(data);
 });
+
+var linkWithMeetup = function(key) {
+	meetup = require('meetup-api')({
+		key: key
+	});
+
+	initApp();
+};
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,35 +32,37 @@ app.use(function(req, res, next) {
 
 var port = process.env.PORT || 9000;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var initApp = function() {
+	// ROUTES FOR OUR API
+	// =============================================================================
+	var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-router.get('/getEvents', function(req, res) {
-	console.log(req.query.zip);
-    meetup.getOpenEvents({
-		zip: req.query.zip
-	}, function(error, events) {
-		if (error) {
-			console.log(error);
-		} else {
-			res.json(events);
-		}
+	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+	router.get('/', function(req, res) {
+	    res.json({ message: 'hooray! welcome to our api!' });
 	});
-});
 
-// more routes for our API will happen here
+	router.get('/getEvents', function(req, res) {
+		console.log(req.query.zip);
+	    meetup.getOpenEvents({
+			zip: req.query.zip
+		}, function(error, events) {
+			if (error) {
+				console.log(error);
+			} else {
+				res.json(events);
+			}
+		});
+	});
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+	// more routes for our API will happen here
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+	// REGISTER OUR ROUTES -------------------------------
+	// all of our routes will be prefixed with /api
+	app.use('/api', router);
+
+	// START THE SERVER
+	// =============================================================================
+	app.listen(port);
+	console.log('Magic happens on port ' + port);
+};
